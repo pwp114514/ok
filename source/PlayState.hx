5164e68f2f408f2a42990f7e9f052f22c59b8e3c
pwp114514 <117086223+pwp114514@users.noreply.github.com>
2023-02-28 13:22:00 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1267,14 +1267,7 @@ class PlayState extends MusicBeatState
 	}
 
 
-	public function setChrome(chromeOffset:Float)
-	{
-		chromeoffsetthing = chromeOffset;
-		chromaticAberration.shader.data.rOffset.value = [chromeOffset];
-		chromaticAberration.shader.data.gOffset.value = [0.0];
-		chromaticAberration.shader.data.bOffset.value = [chromeOffset * -1];
-	}
-
+	
 	public function addShaderToCamera(cam:String,effect:ShaderEffect){
 		switch(cam.toLowerCase()) {
 			case 'camhud' | 'hud':
