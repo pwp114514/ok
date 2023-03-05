a4231458d91154b7f0ba403430dd2037c60ac4e6
pwp114514 <117086223+pwp114514@users.noreply.github.com>
2023-03-05 18:41:10 +0800

    Update ShadersHandler.hx

--- source/ShadersHandler.hx
+++ source/ShadersHandler.hx
@@ -1,12 +1,6 @@
 package;
 
 import openfl.filters.ShaderFilter;
-import flixel.system.FlxAssets.FlxShader;
-import openfl.display.BitmapData;
-import openfl.display.ShaderInput;
-import openfl.utils.Assets;
-import flixel.FlxG;
-import openfl.Lib;
 
 class ShadersHandler
 {
@@ -19,33 +13,5 @@ class ShadersHandler
 		chromaticAberration.shader.data.rOffset.value = [chromeOffset];
 		chromaticAberration.shader.data.gOffset.value = [0.0];
 		chromaticAberration.shader.data.bOffset.value = [chromeOffset * -1];
-	}
-}
-class ChromaticAberration extends FlxShader
-{
-	@:glFragmentSource('
-		#pragma header
-
-		uniform float rOffset;
-		uniform float gOffset;
-		uniform float bOffset;
-
-		void main()
-		{
-			vec4 col = vec4(1.0);
-			
-			col.r = texture2D(bitmap, openfl_TextureCoordv - vec2(rOffset, 0.0)).r;
-			col.ga = texture2D(bitmap, openfl_TextureCoordv - vec2(gOffset, 0.0)).ga;
-			col.b = texture2D(bitmap, openfl_TextureCoordv - vec2(bOffset, 0.0)).b;
-
-			gl_FragColor = col;
-		}')
-	public function new()
-	{
-		super();
-
-		rOffset.value = [0.0];
-		gOffset.value = [0.0];
-		bOffset.value = [0.0];
 	}
 }
